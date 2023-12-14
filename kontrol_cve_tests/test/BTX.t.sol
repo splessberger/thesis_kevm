7385f4684445d6d6555bed1765c818d3e6d1f2ab
Sascha Plessberger <e1425186@student.tuwien.ac.at>
2023-12-14 12:20:42 +0100

    new

--- kontrol_cve_tests/test/BTX.t.sol
+++ kontrol_cve_tests/test/BTX.t.sol
@@ -25,25 +25,26 @@ contract BTXTest is Test, KontrolCheats {
 		vm.assume(bob != address(0));
 		vm.assume(bob != address(token));
 		vm.assume(bob != address(this));
+		vm.assume(bob != alice);
 		vm.assume(totalSupply > 0);
 		vm.assume(amount > 0);
 		vm.assume(balanceBob >= amount);
 		
-		vm.assume(amount < MAX_INT);
-		vm.assume(totalSupply > 0);
-		vm.assume(totalSupply < MAX_INT);
-		vm.assume(balanceAlice > 0);
-		vm.assume(balanceAlice < MAX_INT);
-		vm.assume(balanceBob > 0);
-		vm.assume(balanceBob < MAX_INT);
-		vm.assume((balanceAlice + amount) < MAX_INT);
-		vm.assume((balanceBob - amount) > 0);
-		vm.assume(balanceAlice < totalSupply);
-		vm.assume(balanceBob < totalSupply);
+		// vm.assume(amount < MAX_INT);
+		// vm.assume(totalSupply > 0);
+		// vm.assume(totalSupply < MAX_INT);
+		// vm.assume(balanceAlice > 0);
+		// vm.assume(balanceAlice < MAX_INT);
+		// vm.assume(balanceBob > 0);
+		// vm.assume(balanceBob < MAX_INT);
+		// vm.assume((balanceAlice + amount) < MAX_INT);
+		// vm.assume((balanceBob - amount) > 0);
+		// vm.assume(balanceAlice < totalSupply);
+		// vm.assume(balanceBob < totalSupply);
 
-		vm.assume((amount + balanceAlice) <= (balanceAlice + balanceBob));
-		vm.assume((balanceAlice + balanceBob) <= totalSupply);
-		vm.assume(totalSupply <= MAX_INT);
+		// vm.assume((amount + balanceAlice) <= (balanceAlice + balanceBob));
+		// vm.assume((balanceAlice + balanceBob) <= totalSupply);
+		// vm.assume(totalSupply <= MAX_INT);
 
 		vm.assume((balanceAlice + amount) <= MAX_INT); //Arithmetic Overflow guard
 		vm.assume((balanceBob - amount) >= 0); //Arithmetic Underflow guard
@@ -56,15 +57,15 @@ contract BTXTest is Test, KontrolCheats {
 
 		assertEq(token.balanceOf(alice), newBalanceAlice);
 		assertEq(token.balanceOf(bob), newBalanceBob);
-		assert(alice != address(0));
-		assert(alice != address(token));
-		assert(alice != address(this));
-		assert(bob != address(0));
-		assert(bob != address(token));
-		assert(bob != address(this));
+		assertNotEq(alice, address(0));
+		assertNotEq(alice, address(token));
+		assertNotEq(alice, address(this));
+		assertNotEq(bob, address(0));
+		assertNotEq(bob, address(token));
+		assertNotEq(bob, address(this));
 
-		assert((amount + balanceAlice) <= (balanceAlice + balanceBob));
-		assert((balanceAlice + balanceBob) <= totalSupply);
-		assert(totalSupply <= MAX_INT);
+		assertLe((amount + balanceAlice), (balanceAlice + balanceBob));
+		assertLe((balanceAlice + balanceBob), totalSupply);
+		assertLe(totalSupply, MAX_INT);
     }
 }
