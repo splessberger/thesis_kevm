709f1533942058fa24a58ab9c7ef65cbfbee85a6
sascha <e1425186@student.tuwien.ac.at>
2024-03-25 10:27:43 +0100

    CVE Test working

--- kontrol_cve_tests/test/BTX.t.sol
+++ kontrol_cve_tests/test/BTX.t.sol
@@ -8,23 +8,24 @@ import "kontrol-cheatcodes/KontrolCheats.sol";
 contract BTXTest is Test, KontrolCheats {
     uint256 constant MAX_INT = 2**256 - 1;
     Bittelux token; // Contract under test
+	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
 
-    modifier symbolic() {
-        kevm.symbolicStorage(address(token));
-        _;
-    }
+    // modifier symbolic() {
+    //     kevm.symbolicStorage(address(token));
+    //     _;
+    // }
 
     function setUp() public {
         token = new Bittelux();
     }
-
-    modifier unchangedStorage(bytes32 storageSlot) {
-        bytes32 initialStorage = vm.load(address(token), storageSlot);
-        _;
-        bytes32 finalStorage = vm.load(address(token), storageSlot);
-        assertEq(initialStorage, finalStorage);
-    }
+	
+ 	// modifier unchangedStorage(bytes32 storageSlot) {
+    //     bytes32 initialStorage = vm.load(address(token), storageSlot);
+    //     _;
+    //     bytes32 finalStorage = vm.load(address(token), storageSlot);
+    //     assertEq(initialStorage, finalStorage);
+    // }
 
     function hashedLocation(address _key, bytes32 _index) public pure returns(bytes32) {
         // Returns the index hash of the storage slot of a map at location `index` and the key `_key`.
@@ -32,58 +33,54 @@ contract BTXTest is Test, KontrolCheats {
         return keccak256(abi.encode(_key, _index));
     }
 
-    function testTransfer(address bob, address alice, uint256 amount, bytes32 storageSlot) public symbolic unchangedStorage(storageSlot) {
-    	//kevm.infiniteGas();
+    function testTransfer(address alice, address bob, uint256 amount) public {
+    	kevm.infiniteGas();
 		
-		uint256 totalSupply = token.totalSupply();
-		uint256 balanceAlice = token.balanceOf(alice);
-		uint256 balanceBob = token.balanceOf(bob);
-		vm.assume(alice != address(0));
-		vm.assume(alice != address(token));
-		vm.assume(alice != address(this));
 		vm.assume(bob != address(0));
 		vm.assume(bob != address(token));
 		vm.assume(bob != address(this));
-		vm.assume(bob != alice);
-		vm.assume(notBuiltinAddress(alice));
+		vm.assume(alice != address(0));
+		vm.assume(alice != address(token));
+		vm.assume(alice != address(this));
+		vm.assume(address(0) != address(this));
+		vm.assume(alice != bob);
 		vm.assume(notBuiltinAddress(bob));
-		vm.assume(totalSupply > 0);
+		vm.assume(notBuiltinAddress(alice));
+		vm.assume(token.totalSupply() > 0);
 		vm.assume(amount > 0);
 
-        bytes32 storageLocationAlice = hashedLocation(alice, 0);
-        bytes32 storageLocationBob = hashedLocation(bob, 0);
-        vm.assume(storageLocationAlice != storageLocationBob);
+        // bytes32 storageLocationBob = hashedLocation(bob, 0);
+        // bytes32 storageLocationAlice = hashedLocation(alice, 0);
+        // vm.assume(storageLocationBob != storageLocationAlice);
 		
-		// vm.assume(amount < MAX_INT);
-		// vm.assume(totalSupply > 0);
-		// vm.assume(totalSupply < MAX_INT);
-		vm.assume(balanceAlice > 0);
-		// vm.assume(balanceAlice < MAX_INT);
-		vm.assume(balanceBob > 0);
+		// vm.assume(token.totalSupply() < MAX_INT);
+		// vm.assume(token.balanceOf(bob) > 0);
 		// vm.assume(balanceBob < MAX_INT);
-		vm.assume((balanceAlice + amount) < MAX_INT);
-		// vm.assume((balanceBob - amount) > 0);
-		// vm.assume(balanceAlice < totalSupply);
+		// vm.assume(token.balanceOf(alice) > 0);
+		// vm.assume(balanceAlice < MAX_INT);
+		// vm.assume((token.balanceOf(alice) + amount) < MAX_INT);
+		// vm.assume((balanceAlice - amount) > 0);
 		// vm.assume(balanceBob < totalSupply);
+		// vm.assume(balanceAlice < totalSupply);
 
-		vm.assume((amount + balanceAlice) <= (balanceAlice + balanceBob));
-		vm.assume((balanceAlice + balanceBob) <= totalSupply);
-		vm.assume(totalSupply <= MAX_INT);
+		vm.assume((amount + token.balanceOf(bob)) <= (token.balanceOf(alice) + token.balanceOf(bob)));
+		vm.assume((token.balanceOf(alice) + token.balanceOf(bob)) <= token.totalSupply());
+		vm.assume(token.totalSupply() <= MAX_INT);
 
-		vm.assume(balanceAlice <= (MAX_INT - amount)); //Arithmetic Overflow guard
-		vm.assume(balanceBob >= amount); //Arithmetic Underflow guard
+		// vm.assume(token.balanceOf(bob) < (MAX_INT - amount)); //Arithmetic Overflow guard
+		// vm.assume(token.balanceOf(alice) > amount); //Arithmetic Underflow guard
 
-		uint256 newBalanceAlice = (balanceAlice + amount);
-		uint256 newBalanceBob = (balanceBob - amount);
+		uint256 newBalanceBob = (token.balanceOf(bob) + amount);
+		uint256 newBalanceAlice = (token.balanceOf(alice) - amount);
 
-		vm.prank(bob);
-		token.transfer(alice, amount);
+		vm.prank(alice);
+		token.transfer(bob, amount);
 
-		assertEq(token.balanceOf(alice), newBalanceAlice);
 		assertEq(token.balanceOf(bob), newBalanceBob);
+		assertEq(token.balanceOf(alice), newBalanceAlice);
 
-		//assertLe((amount + balanceAlice), (balanceAlice + balanceBob));
-		assertLe((token.balanceOf(alice) + token.balanceOf(bob)), totalSupply);
-		assertLe(totalSupply, MAX_INT);
+		assertLe(token.balanceOf(bob), (token.balanceOf(alice) + token.balanceOf(bob)));
+		assertLe((token.balanceOf(alice) + token.balanceOf(bob)), token.totalSupply());
+		assertLe(token.totalSupply(), MAX_INT);
     }
 }
